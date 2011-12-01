f9ac9e9d7084c77689ec878c69858d9f3ac5aeb6
misc utils <you@example.com>
2011-12-01 20:02:31 +0100

    init

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='filestube',
+      version='0.1',
+      description='short description',
+      author='you',
+      long_description='long description',
+      # or you can also do something like this
+      # long_description=open("README").read(),
+      author_email='you@example.com',
+      url='http://example.com',
+      # list of packages you want you application/lib install
+      license= 'the licence you are distributing your software under',
+      # list of scripts supplied by your application
+      scripts=['ft'],
+      keywords='a series of keywords separates by spaces',
+     )
+
+# vim:set shiftwidth=4 tabstop=4 expandtab:
